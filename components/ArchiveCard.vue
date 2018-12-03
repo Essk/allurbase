31f30163486e6acbcb04d311cc56be93e01e18ed
Sarah Rimron-Soutter <sarah@teaandbiscuits.net>
2018-12-03 22:28:13 +0000

    starting to use bootstrap built-ins

---
+++ components/ArchiveCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-media>
+    <b-img slot="aside" blank blank-color="#ccc" width="64" alt="placeholder"/>
+    <h5 class="mt-0">{{ title }}</h5>
+    <div v-html="content"/>
+    <div class="archive-card__meta">
+      <slot name="card-meta"/>
+    </div>
+  </b-media>
+</template>
+
+<script>
+export default {
+  name: 'ArchiveCard',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    content: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
